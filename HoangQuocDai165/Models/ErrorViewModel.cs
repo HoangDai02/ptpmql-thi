c6682154989e48ad5bbb29b01398edfc40b1a167
HoangDai02 <dai.hoang668@gmail.com>
2024-06-16 16:13:08 +0700

    newbie

---
+++ HoangQuocDai165/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace HoangQuocDai165.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
